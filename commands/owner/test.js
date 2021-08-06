459a8f43615689bb66a135acd887177128df9091
John Gooden <johnnyg3587@gmail.com>
2021-08-06 11:35:43 -0500

    remove discord buttons since discord.js supports them now

--- commands/owner/test.js
+++ commands/owner/test.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { MessageButton } = require('discord.js');
 const fs = require("fs")
 
 module.exports = {
@@ -7,10 +8,13 @@ module.exports = {
     usage: ``, //usage instructions w/o command name and prefix
 	cooldown: 1, //cooldown in seconds, defaults to 3
 	permissions: [], //permissions required for command
-    disabled: true,
 	ownerOnly: true, //need to be the owner? delete line if no
 	aliases: [],
 	execute(message, args, prefix) { //inside here command stuff
-        
+        const button = new Discord.MessageButton()
+		.setCustomid('testbtn')
+		.setLabel('Push Me')
+		.setStyle('RED')
+		message.channel.send('heyo', button)
 	},
 };
