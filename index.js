9a9b5595f059a82602f2c3166750db52e214a0ef
John Gooden <johnnyg3587@gmail.com>
2021-08-07 11:16:35 -0500

    another test push

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ A mascot and quality of life discord bot, mainly for the purpose of entertaining
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-
 const { Client, Intents, Collection} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 const path = require("path")
