512b7816b87297666c958f86f1aeb2a03fba7c5c
John Gooden <johnnyg3587@gmail.com>
2021-09-09 15:40:49 -0500

    invite/vote command

---
+++ commands/general/invite.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const { invite } = require('../../config.json');
+
+module.exports = {
+	name: 'invite', //command name
+	description: 'Get an invite link to the bot.', //command description
+    usage: ``, //usage instructions w/o command name and prefix
+	cooldown: 5, //cooldown in seconds, defaults to 3
+	permissions: [], //permissions required for command
+	aliases: ['vote'],
+	execute(message, args, prefix) { //inside here command stuff
+		let inviteembed = new Discord.MessageEmbed()
+        .setTitle('Invite or vote here!')
+        .setDescription(`Vote for me at [discordbotlist.com](https://discordbotlist.com/bots/harold)\nInvite me [here.](${invite})`)
+        .setColor('RANDOM')
+        message.channel.send({ embeds: [inviteembed]})
+	},
+};
