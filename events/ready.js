775c8b1c5fd660b1452aeeaf94e2804f6d2b6ac9
John Gooden <johnnyg3587@gmail.com>
2021-09-26 17:02:30 -0500

    Bot now shows latest commit on start.

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
+const got = require('got');
+
 module.exports = {
 	name: 'ready', //name, duh
 	once: true, //remove if false 
@@ -5,5 +7,11 @@ module.exports = {
 		console.info(`Ready at: ${client.readyAt}`)
     	console.info('Harold Bot Copyright (C) 2021  John Gooden')
     	console.info('Copyright info: https://github.com/johng3587/Harold/blob/main/LICENCE\n\n')
+		got('https://api.github.com/repos/johng3587/Harold/commits')
+		.then(response => {
+			let commits = JSON.parse(response.body)
+			let latest = commits[0]
+			client.user.setActivity(`Latest update: ${latest.commit.message}`)
+		})
 	},
 };
