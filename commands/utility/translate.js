c6d5067adbcf5e70c8941e4f141d70221a154727
johng3587 <57050681+johng3587@users.noreply.github.com>
2021-09-27 07:48:47 -0500

    translate command public

--- commands/utility/translate.js
+++ commands/utility/translate.js
@@ -7,7 +7,6 @@ module.exports = {
     usage: `(Reply to a message)`, //usage instructions w/o command name and prefix
 	cooldown: 5, //cooldown in seconds, defaults to 3
 	permissions: [], //permissions required for command
-	ownerOnly: true, //need to be the owner? delete line if no
 	aliases: [],
 	async execute(message, args, prefix) { //inside here command stuff
 		if(!message.reference) return(message.reply('You didn\'t reply to a message!'))
