e340a602b0e13fc5573029e3655a1ebf6f3cc112
John Gooden <johnnyg3587@gmail.com>
2021-11-01 17:50:11 -0500

    slash command prepping

--- commands/owner/ownerhelp.js
+++ commands/owner/ownerhelp.js
@@ -8,10 +8,17 @@ module.exports = {
 	ownerOnly: true, //need to be the owner? delete line if no
 	aliases: ['ownercommands'],
 	execute(message, args, prefix) { //inside here command stuff
+		const data = [];
+		const { commands } = message.client;
+
+		data.push(commands.filter(command => command.ownerOnly).map(command => command.name).join('\n'));
+        data.push(`\n**\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+		let description = '**' + data
+		
 		const ownerembed = new Discord.MessageEmbed()
 		.setTitle('Owner Help Menu')
 		.setColor('RANDOM')
-		.setDescription('**setstatus** - sets the bot presence\n**setavatar** - sets the bot avatar\n**setgame** - sets the game the bot is playing\n**log** - logs information to the console\n**shutdown** - shuts down the bot\n**ownerhelp** - displays this embed\n**setstream** <url> <name> - sets a streaming status\n**block** - make the bot ignore someone\n**unblock** - make the bot resume working for someone\n**hibernate** - makes the bot go into low-power mode\n**hibernate_off** - turns off hibernate and enables normal bot functions\n**reload** <command/alias> - reloads a command after code changes')
+		.setDescription(description, { split: true})
 		message.react('📬')
 		message.author.send ({ embeds: [ownerembed]})
 	},
