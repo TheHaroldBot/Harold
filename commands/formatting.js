2fcf4acf146ce2ba252a4783c27c04639f675cc3
johng3587 <57050681+johng3587@users.noreply.github.com>
2021-11-09 08:17:49 -0600

    final formatting command update

--- commands/formatting.js
+++ commands/formatting.js
@@ -15,12 +15,13 @@ module.exports = {
         const formatembed = new Discord.MessageEmbed()
             .setTitle('A handy guide to Discord formatting')
             .addFields(
-                { name: 'Text Formatting', value: '\\*\\*bold\\*\\*n\\*italic\\*\n\\_\\_underline\\_\\_\n\\~\\~strikethrough\\~\\~\n\\*\\*\\*bold and italic\\*\\*\\*\n\\*\\*\\*\\_\\_\\~\\~bold, italic, underlined, and strikethrough\\~\\~\\_\\_\\*\\*\\*\n\\|\\|spoiler\\|\\|'},
+                { name: 'Text Formatting', value: '\\*\\*bold\\*\\*\n\\*italic\\*\n\\_\\_underline\\_\\_\n\\~\\~strikethrough\\~\\~\n\\*\\*\\*bold and italic\\*\\*\\*\n\\*\\*\\*\\_\\_\\~\\~bold, italic, underlined, and strikethrough\\~\\~\\_\\_\\*\\*\\*\n\\|\\|spoiler\\|\\|'},
                 { name: 'Quote Formatting', value: '\\> - quote a single line\n\\>\\>\\> - quote until end of message'},
                 { name: 'Mention Formatting', value: '<@userId> - mention a user\n<@&roleId> - mention a role\n<#channelId> - mention a channel'},
                 { name: 'Code Formatting', value: '\\\`Single line code block, no color\\\`\n\\\`\\\`‎\\\`codingLanguage\nMulti-line code block, with language color\n\\\`\\\`‎\\\`'},
                 { name: 'Time Formatting', value: '<t:unixTimestamp:d> - send a timestamp in mm/dd/yyyy format\n<t:unixTimestamp:D> - send a timestamp in Month Day, Year format\n<t:unixTimestamp:t> - send a timestamp in hh:mm AM/PM format\n<t:unixTimestamp:T> - send a timestamp in hh:mm:ss AM/PM format\n<t:unixTimestamp:f> - send a timestamp in Month Day, Year hh:mm AM/PM format\n<t:unixTimestamp> - send a timestamp in Month Day, Year hh:mm AM/PM format\n<t:unixTimestamp:F> - send a timestamp in Weekday, Month Day, Year hh:mm AM/PM format\n<t:unixTimestamp:R> - send a timestamp in relative format\nunixTimestamp - send a timestamp in unix format\nConverter at [hammertime](https://hammertime.djdavid98.art/)'}
             )
+            .setColor('RANDOM')
         message.author.send({embeds: [formatembed]})
         message.react('📬')
     },
