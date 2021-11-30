d654e0435ca6bcb137f47223b532408eda02eab4
John Gooden <johnnyg3587@gmail.com>
2021-11-29 19:20:53 -0600

    guildCreate event error catches

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -11,7 +11,12 @@ module.exports = {
 			.setColor('RANDOM')
 			.setDescription(`Thank you for adding me to your server!\nRun \`${prefix}help\` to get my commands!\nThings to know: I am still under developement, and will have a few bugs, feel free to report them with \`${prefix}bugreport\`.\nMy GitHub can be found [here](https://github.com/johng3587/Harold).`);
 		const owner = await guild.fetchOwner();
-		owner.send({ embeds: [introembed] }).catch(console.error());
+		try {
+			await owner.send({ embeds: [introembed] }).catch(console.error());
+		}
+		catch (error) {
+			console.error(`Error sending into embed to ${owner.tag} from ${guild.name}:\n${error}`);
+		}
 		console.info(`I just joined a new server! I am now a member of ${guild.name}`);
 	},
 };
