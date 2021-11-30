3831ac30d4723b13f8ed4540ae68db9b2e1940e3
John Gooden <johnnyg3587@gmail.com>
2021-11-29 19:41:01 -0600

    block and unblock combined

--- commands/block.js
+++ commands/block.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
+const removeFromArray = require('remove-from-array');
 
 module.exports = {
 	name: 'block', // command name
 	description: 'Blocks a user from using the bot', // command description
 	args: true, // needs arguments? delete line if no
-	usage: '<mention>', // usage instructions w/o command name and prefix
+	usage: '<add|remove> <mention>', // usage instructions w/o command name and prefix
 	cooldown: 1, // cooldown in seconds, defaults to 3
 	permissions: [], // permissions required for command
 	ownerOnly: true, // need to be the owner? delete line if no
 	aliases: [],
-	execute(message) { // inside here command stuff
-		const data = JSON.parse(fs.readFileSync('././config.json'));
-		if (data.blocked.includes(message.mentions.users.first().id)) return (message.channel.send('That person is already blocked.'));
-		data.blocked.push(message.mentions.users.first().id);
-		fs.writeFileSync('././config.json', JSON.stringify(data));
-		message.channel.send(`Successfully blocked ${message.mentions.users.first().tag}.`);
+	execute(message, args, prefix) { // inside here command stuff
+		if (args.length < 2) return message.channel.send(`You need to specify an action and a user to block! \`${prefix}${this.name} ${this.usage}\``);
+		if (args[0] === 'add') {
+			const data = JSON.parse(fs.readFileSync('././config.json'));
+			if (data.blocked.includes(message.mentions.users.first().id)) return (message.channel.send('That person is already blocked.'));
+			data.blocked.push(message.mentions.users.first().id);
+			fs.writeFileSync('././config.json', JSON.stringify(data));
+			message.channel.send(`Successfully blocked ${message.mentions.users.first().tag}.`);
+		}
+		else if (args[0] === 'remove') {
+			const data = JSON.parse(fs.readFileSync('././config.json'));
+			if (!data.blocked.includes(message.mentions.users.first().id)) return (message.channel.send('That person is not blocked.'));
+			removeFromArray(data.blocked, message.mentions.users.first().id);
+			fs.writeFileSync('././config.json', JSON.stringify(data));
+			message.channel.send(`Successfully unblocked ${message.mentions.users.first().tag}.`);
+		}
 	},
 };
