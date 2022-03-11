a804c735c85a9db4fe062b5681f68b558a39fb0f
John Gooden <johnnyg3587@gmail.com>
2022-03-11 00:14:50 -0600

    public publishing works now

--- publishCommandsPublic.js
+++ publishCommandsPublic.js
@@ -20,7 +20,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(botid),
+			Routes.applicationCommands(botid),
 			{ body: commands },
 		);
 
