ceb9eaefac126984e6d53e1ef555fc3566aebd69
John Gooden <johnnyg3587@gmail.com>
2022-04-03 21:10:44 -0500

    functions testing

--- functions.js
+++ functions.js
@@ -1,4 +1,16 @@
 /* eslint-disable no-undef */
 // figure this out pls
 
+function function1() {
+	return ('Hello world!');
+}
+
+function function2(name) {
+	return ('Hello ' + name + '!');
+}
+
+function function3(name) {
+	return ('Goodbye ' + name + '!');
+}
+
 module.exports = { function1, function2, function3 };
