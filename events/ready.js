f0d956b02881abce6cdd35fa79e42383d8219f2d
John Gooden <johnnyg3587@gmail.com>
2022-04-11 19:25:05 -0500

    license changes

--- events/ready.js
+++ events/ready.js
@@ -5,8 +5,8 @@ module.exports = {
 	once: true, // remove if false
 	execute(client) { // stuff to do
 		console.info(`Ready at: ${client.readyAt}`);
-		console.info('Harold Bot Copyright (C) 2021  John Gooden');
-		console.info('Copyright info: https://github.com/johng3587/Harold/blob/main/LICENCE\n\n');
+		console.info('Harold Bot Copyright (C) 2022  John Gooden');
+		console.info('Copyright info: https://github.com/johng3587/Harold/blob/main/LICENSE\n\n');
 		got('https://api.github.com/repos/johng3587/Harold/commits')
 			.then(response => {
 				const commits = JSON.parse(response.body);
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // https://github.com/johng3587/Harold
 
 /*
-    A mascot and quality of life discord bot, mainly for the purpose of entertaining people.
+    Harold is a discord bot I made primarily for fun.
     Copyright (C) 2022  John Gooden
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms listed in the LICENSE file.
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+	Questions, comments, or concerns? Contact me at: johnnyg3587@gmail.com
 */
 
 const { Client, Intents, Collection } = require('discord.js');
