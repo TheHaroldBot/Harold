78dd023917446fdf96867d62ae87c518466744d7
John Gooden <johnnyg3587@gmail.com>
2022-04-26 22:14:30 -0500

    invite command deprecated, replaced with botstats

--- commands/botstats.js
+++ commands/botstats.js
@@ -3,21 +3,23 @@ const Discord = require('discord.js');
 const { invite } = require('../config.json');
 
 module.exports = {
-	name: 'invite', // command name
-	description: 'Get an invite link to the bot.', // command description
+	name: 'botstats', // command name
+	description: 'Get bot stats and info', // command description
 	usage: '', // usage instructions w/o command name and prefix
 	cooldown: 5, // cooldown in seconds, defaults to 3
 	permissions: [], // permissions required for command
+	disabled: false, // whether the command is disabled
+	ownerOnly: true, // temp
 	myPermissions: ['SEND_MESSAGES'], // permissions bot needs for command
 	aliases: ['vote'],
 	data: new SlashCommandBuilder()
-		.setName('invite')
-		.setDescription('Get an invite link to the bot.'),
+		.setName('botstats')
+		.setDescription('Get bot stats and info'),
 
 	execute(interaction) { // inside here command stuff
 		const inviteembed = new Discord.MessageEmbed()
 			.setTitle('Invite or vote here!')
-			.setDescription(`Vote for me at [discordbotlist.com](https://discordbotlist.com/bots/harold)\nInvite me [here.](${invite})`)
+			.setDescription(`🏓 Latency is ${interaction.client.ws.ping}ms\nWe are in ${interaction.client.guilds.cache.size} guilds! \nVote for me at [discordbotlist.com](https://discordbotlist.com/bots/harold)\nInvite me [here.](${invite})`)
 			.setColor('RANDOM');
 		interaction.reply({ embeds: [inviteembed] });
 	},
