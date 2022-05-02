b30d10a9138d7e3ca5b8970d8a99fe6b37d13837
John Gooden <johnnyg3587@gmail.com>
2022-05-01 20:38:21 -0500

    setavatar merged into set

--- commands/set.js
+++ commands/set.js
@@ -1,17 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-	name: 'setstatus', // command name
+	name: 'set', // command name
 	description: 'Sets the bot\'s presence.', // command description
 	args: true, // needs arguments? delete line if no
-	usage: '<online|invisible|dnd|idle>', // usage instructions w/o command name and prefix
+	usage: '<presence|activity|avatar>', // usage instructions w/o command name and prefix
 	cooldown: 5, // cooldown in seconds, defaults to 3
 	permissions: [], // permissions required for command
 	myPermissions: ['SEND_MESSAGES'], // permissions bot needs for command
 	ownerOnly: true, // need to be the owner? delete line if no
 	aliases: [],
 	data: new SlashCommandBuilder()
-		.setName('setstatus')
+		.setName('set')
 		.setDescription('Sets the bot\'s status')
 		.addSubcommand(subcommand =>
 			subcommand.setName('presence')
@@ -80,18 +80,48 @@ module.exports = {
 						.setRequired(false)
 						.setDescription('The activity url.'),
 				),
+		)
+		.addSubcommand(subcommand =>
+			subcommand.setName('avatar')
+				.setDescription('Sets the bot\'s avatar.')
+				.addStringOption(option =>
+					option.setName('url')
+						.setRequired(true)
+						.setDescription('The avatar url.'),
+				),
 		),
-	execute(interaction) { // inside here command stuff
+	async execute(interaction) { // inside here command stuff
 		if (interaction.options.getSubcommand() === 'presence') {
-			interaction.client.user.setPresence({ status: interaction.options.getString('presence') });
-			interaction.reply(`Status set to ${interaction.options.getString('presence')}`);
+			try {
+				await interaction.client.user.setPresence({ status: interaction.options.getString('presence') });
+				await interaction.reply(`Status set to ${interaction.options.getString('presence')}`);
+			}
+			catch (error) {
+				const returnError = { message: error.message, stack: error.stack, code: 500, report: true, myMessage: 'Uh-oh, something went wrong when setting the presence!' };
+				throw returnError;
+			}
 		}
 		else if (interaction.options.getSubcommand() === 'activity') {
-			interaction.client.user.setActivity(interaction.options.getString('name'), {
-				type: interaction.options.getString('type'),
-				url: interaction.options.getString('url'),
-			});
-			interaction.reply(`Activity set to ${interaction.options.getString('type')} ${interaction.options.getString('name')}`);
+			try {
+				await interaction.client.user.setActivity(interaction.options.getString('name'), {
+					type: interaction.options.getString('type'),
+					url: interaction.options.getString('url'),
+				});
+				await interaction.reply(`Activity set to ${interaction.options.getString('type')} ${interaction.options.getString('name')}`);
+			}
+			catch (error) {
+				const returnError = { message: error.message, stack: error.stack, code: 500, report: true, myMessage: 'Uh-oh, something went wrong when setting a game!' };
+				throw returnError;
+			}
+		}
+		else if (interaction.options.getSubcommand() === 'avatar') {
+			try {
+				await interaction.client.user.setAvatar(interaction.options.getString('url'));
+				await interaction.reply(`Avatar set to ${interaction.options.getString('url')}`);
+			}
+			catch (error) {
+				interaction.reply('Error setting avatar, check your URL and try again.');
+			}
 		}
 
 	},
