d6506187f9d66d064298ad55eebbce8a29f9aa98
John Gooden <johnnyg3587@gmail.com>
2025-04-28 08:50:57 -0500

    Fixed insult API, using new API

--- commands/insult.js
+++ commands/insult.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 module.exports = {
 	name: 'insult', // command name
-	description: 'Insults you.', // command description
+	description: 'Insults you, in a pirate fashion.', // command description
 	usage: '', // usage instructions w/o command name and prefix
 	cooldown: 0.5, // cooldown in seconds, defaults to 3
 	myPermissions: [PermissionFlagsBits.SendMessages], // permissions bot needs for command
@@ -14,12 +14,21 @@ module.exports = {
 
 	async execute(interaction) { // inside here command stuff
 		const insultsettings = { method: 'Get' };
-		const insulturl = 'https://insult.mattbas.org/api/insult.json'; // insult api
+		// const insulturl = 'https://insult.mattbas.org/api/insult.json'; // insult api
+		const insulturl = 'https://pirate.monkeyness.com/api/insult'; // new one since the last stopped working
 		try {
-			await fetch(insulturl, insultsettings)
+			/* await fetch(insulturl, insultsettings)
 				.then(res => res.json())
 				.then((json) => {
 					interaction.reply(json.insult);
+				})
+				.catch(err => {
+					throw new Error(err.stack);
+				}); */
+			await fetch(insulturl, insultsettings)
+				.then(res => res.text())
+				.then((res) => {
+					interaction.reply(res);
 				})
 				.catch(err => {
 					throw new Error(err.stack);
